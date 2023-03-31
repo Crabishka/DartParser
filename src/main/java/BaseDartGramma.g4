e8e9903728a093494c6dce30b50cf963d14648f6
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-03-31 16:14:22 +0300

    fix regex (not)

--- src/main/java/BaseDartGramma.g4
+++ src/main/java/BaseDartGramma.g4
@@ -1,11 +1,13 @@
 grammar BaseDartGramma;
 
-value: [0-9]* | '"' [a-zA-Z0-9]* '"';
+value: digit* | '"' (symbol | digit | letter)* '"';
 
 identifier :  letter (letter | digit | '_' )* '?'? ;
 
-letter : [a-zA-Z];
+letter :'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'  ;
 
-digit: [0-9];
+symbol : '[' | '\\' | ']' | '^' | '_' | '`' | ' ' | '!' | '?' | '.' ;
+
+digit: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0'  ;
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import gen.DartGrammaLexer;
-import gen.DartGrammaParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.*;
@@ -8,17 +6,5 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        FileInputStream inputStream = new FileInputStream("src/main/resources/func.dart");
-
-        CharStream input = CharStreams.fromStream(inputStream);
-
-        DartGrammaLexer lexer = new DartGrammaLexer(input);
-
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        DartGrammaParser parser = new DartGrammaParser(tokens);
-
-        ParseTree tree = parser.compilationUnit();
-        System.out.println(tree.toStringTree(parser));
     }
 }
